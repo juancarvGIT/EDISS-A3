d0a3a9e269191394dc98013b46aebd31bfe08f1f
juancarvGIT <165414295+juancarvGIT@users.noreply.github.com>
2024-04-21 22:21:48 -0600

    Update RecommendationEngineService.java

--- A3_submit/a3-book-store-service/src/main/java/edu/cmu/andrew/application/bussines/RecommendationEngineService.java
+++ A3_submit/a3-book-store-service/src/main/java/edu/cmu/andrew/application/bussines/RecommendationEngineService.java
@@ -8,12 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import edu.cmu.andrew.application.business.dto.RecommendedBookDTO;
 import edu.cmu.andrew.application.rest.client.FeignRecommendationEngine;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 /**
 * <h1>Recommendation Engine Service</h1>
 * 
-*  Service layer that controls the execution and invocation of the service recommendation engine, it also manages the circuit breaker status
+*  Service layer that controls the execution and invocation of the service recommendation engine, it also manages the circuiteBreaker status
 *  
 * @author  Juan Carlos Villegas Montiel
 * @version 1.0
@@ -26,7 +27,7 @@ public class RecommendationEngineService {
 	FeignRecommendationEngine feignRecommendationEngine;
 	
 	/**
-	 * Method that invokes the recommendation engine, note that is protected by a fallback method called "fallback" in case the engine service is unavailable
+	 * Method that invokes the recommenation engine, note that is protected by a fallback method called "fallback" in case the engine service is unavailable
 	 * @param isbn the book to get recommendations from
 	 * @return a List of recommended books
 	 */
@@ -43,13 +44,22 @@ public class RecommendationEngineService {
 	}
 	
 	/**
-	 * This is a fallback method that will execute only if the recommendation engine service is unavailable or it produces a timeout
+	 * This is a fallback method that will execute only if the recommendation engine service is unavailable or it produced a timeout error previously causing that the circuit opens
 	 * @param e the exception produced by the unsuccessful call to the third party service in this case the recommendation engine
 	 * @return a response entity with the error code 503, service unavailable
 	 */
-	public ResponseEntity<List<RecommendedBookDTO>> fallback(Exception e) {
+	public ResponseEntity<List<RecommendedBookDTO>> fallback(CallNotPermittedException e) {		
 		System.out.println("Fall back:"+e.getMessage());
         return new ResponseEntity<List<RecommendedBookDTO>>(HttpStatus.SERVICE_UNAVAILABLE);
+    }
+	/**
+	 * This is a fallback method that will execute in the case of a general error with the circuit closed, it will handle the timeout in the close state
+	 * @param e the exception produced by the unsuccessful call to the third party service in this case the recommendation engine
+	 * @return a response entity with the error code 504, service unavailable
+	 */
+	public ResponseEntity<List<RecommendedBookDTO>> fallback(Exception e) {		
+		System.out.println("Catch error:"+e.getMessage());
+        return new ResponseEntity<List<RecommendedBookDTO>>(HttpStatus.GATEWAY_TIMEOUT);
     }
 	
 }
